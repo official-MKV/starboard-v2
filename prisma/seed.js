55733805dca2008e1fec2a291d3e7fb12f75d136
official-MKV <vemmaks84@gmail.com>
2025-06-05 07:34:17 +0100

    Push

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,6 @@
 const { PrismaClient } = require('@prisma/client')
 // import { PrismaClient } from '@prisma/client'
 const bcrypt = require('bcryptjs')
-// import bcrypt from 'bcryptjs'
 
 const prisma = new PrismaClient()
 
